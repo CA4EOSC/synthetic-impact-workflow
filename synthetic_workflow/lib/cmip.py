bf0a5cd2003a3ddb43aa33a9ff65e043fad83c59
Paul Slavin <pmslavin@gmail.com>
2026-06-11 16:24:39 +0100

    Add cmip retriever and cache to lib; Add per-Stage entry points

---
+++ synthetic_workflow/lib/cmip.py
@@ -0,0 +1,65 @@
+import os
+import sys
+
+import cdsapi
+
+
+area = [66, -10, 35, 40]  # Boundary lats/longs of European region
+base_start = 1850
+base_end = 1914
+sample_start = 1950
+sample_end = 2104
+
+data_dir = "data"
+
+baseline = {
+   "filename": "cmip6_baseline.zip",
+   "ncfile": "tas_Amon_HadGEM3-GC31-MM_historical_r1i1p1f3_gn_18500116-19141216.nc",
+   "dataset": "projections-cmip6",
+   "request": {
+        "temporal_resolution": "monthly",
+        "experiment": "historical",
+        "variable": "near_surface_air_temperature",
+        "model": "hadgem3_gc31_mm",
+        "year": [f"{y}" for y in range(base_start, base_end+1)],
+        "month": [f"{m:02}" for m in range(1, 13)],
+        "area": area
+    }
+}
+
+sample = {
+    "filename": "cmip6_sample.zip",
+    "ncfile": "tas_Amon_HadGEM3-GC31-MM_historical_r1i1p1f3_gn_19500116-20141216.nc",
+    "dataset": "projections-cmip6",
+    "request": {
+        "temporal_resolution": "monthly",
+        "experiment": "historical",
+        "variable": "near_surface_air_temperature",
+        "model": "hadgem3_gc31_mm",
+        "year": [f"{y}" for y in range(sample_start, sample_end+1)],
+        "month": [f"{m:02}" for m in range(1, 13)],
+        "area": area
+    }
+}
+
+
+def retrieve_datasets():
+    if has_local_datasets():
+        print("Datasets present")
+        sys.exit(0)
+
+    client = cdsapi.Client()
+
+    for subset in (baseline, sample):
+        client.retrieve(subset["dataset"], subset["request"]).download(os.path.join(data_dir, subset["filename"]))
+
+
+def has_local_datasets():
+    for subset in (baseline, sample):
+        if not (os.path.exists(subset["filename"]) or os.path.exists(subset["ncfile"])):
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    retrieve_datasets()
--- synthetic_workflow/main.py
+++ synthetic_workflow/main.py
@@ -1,6 +1,10 @@
+import argparse
+import sys
+
 from .lib.temperature import make_annual_deviations
 from .lib.solver import gauss_newton_fd
 from .lib.population import generate_population_series
+from .lib.cmip import retrieve_datasets
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,14 +13,102 @@ import numpy as np
 np.random.seed(0x20)
 
 
-def main():
+def parse_args(argv):
+    parser = argparse.ArgumentParser(
+        prog="synthetic-ca4eosc-workflow",
+        description="A demonstration synthetic climate impact workflow",
+        epilog="https://github.com/CA4EOSC/synthetic-impact-workflow"
+    )
+
+    parser.add_argument("--retrieve-data",
+        action="store_true",
+        default=False,
+        help="Perform Stage 1 of the Workflow: Data Retrieval"
+    )
+
+    parser.add_argument("--make-deviations",
+        action="store_true",
+        default=False,
+        help="Perform Stage 2 of the Workflow: Calculate Temperature Deviations"
+    )
+
+    parser.add_argument("--make-population",
+        action="store_true",
+        default=False,
+        help="Perform Stage 3 of the Workflow: Generate Synthetic Population"
+    )
+
+    parser.add_argument("--fit-capacities",
+        action="store_true",
+        default=False,
+        help="Perform Stage 4 of the Workflow: Fit Carrying Capacities"
+    )
+
+    parser.add_argument("--make-regression",
+        action="store_true",
+        default=False,
+        help="Perform Stage 5 of the Workflow: Deviation/Capacity Regression"
+    )
+
+    parser.add_argument("--make-visualisation",
+        action="store_true",
+        default=False,
+        help="Perform Stage 6 of the Workflow: Regression Visualisation"
+    )
+
+    parser.add_argument("--run-workflow",
+        action="store_true",
+        default=False,
+        help="Run all Stages of the workflow in order"
+    )
+
+    if len(argv) == 1:
+        parser.print_help()
+        sys.exit(0)
+
+    return parser.parse_args()
+
+
+def handle_args(args):
+    if args.run_workflow:
+        run_workflow()
+
+    if args.retrieve_data:
+        retrieve_datasets()
+
+    if args.make_deviations:
+        annual_deviations = make_annual_deviations()
+
+    if args.make_population:
+        pop = generate_population_series(annual_deviations)
+
+    if args.fit_capacities:
+        params = np.array([1.0, 0.5])
+        rx, ry = [], []
+
+        for idx, dev in enumerate(annual_deviations):
+            y = pop[idx][0:8]
+            x = np.array([*range(8)])
+            fit = gauss_newton_fd(x, y, params)
+            rx.append(dev)
+            ry.append(fit[0])
+
+    if args.make_regression:
+        b, m = np.polynomial.polynomial.polyfit(rx, ry, 1)
+
+    if args.make_visualisation:
+        plt.scatter(rx, ry)
+        plt.axline((0, b), (1, b+m), color="red")
+        plt.xlabel("Temperature Deviation °C")
+        plt.ylabel("Annual Inferred Capacity")
+        plt.show()
+
+
+def run_workflow():
     params = np.array([1.0, 0.5])
 
     annual_deviations = make_annual_deviations()
     pop = generate_population_series(annual_deviations)
-
-    #plt.plot(np.ravel(pop))
-    #plt.show()
 
     rx = []
     ry = []
@@ -35,6 +127,11 @@ def main():
     plt.xlabel("Temperature Deviation °C")
     plt.ylabel("Annual Inferred Capacity")
     plt.show()
+
+
+def main():
+    args = parse_args(sys.argv)
+    handle_args(args)
 
 
 if __name__ == "__main__":
